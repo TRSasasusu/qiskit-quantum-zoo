f121c04576f1dbdd40cab457592d06057b54d911
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2021-11-20 21:40:56 +0900

    Use N more than or equal to M squared.

--- qqz/elementary.py
+++ qqz/elementary.py
@@ -44,75 +44,49 @@ def adder(n: int) -> Gate:
 
     return qc.to_gate()
 
-def adder_modN(N: int, N_len: Optional[int] = None) -> Gate:
-    N_val = N
-    if N_len is None:
-        N_len = int(np.ceil(np.log2(N)))
+def adder_modM(M: int, N_len: int) -> Gate:
+    M_val = M
 
     qubits = QuantumRegister(N_len + (N_len + 1) + N_len + N_len + 1)
     a, left_qubits = qubits[:N_len], qubits[N_len:]
     b, left_qubits = left_qubits[:N_len + 1], left_qubits[N_len + 1:]
     c, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
-    N, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
+    M, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
     t = left_qubits[:1]
     qc = QuantumCircuit(qubits)
 
     adder_gate = adder(N_len)
     adder_gate_dag = adder_gate.inverse()
 
-    for i, char in enumerate(bin(N_val)[2:][::-1]):
+    for i, char in enumerate(bin(M_val)[2:][::-1]):
         if char == '1':
-            qc.x(N[i])
+            qc.x(M[i])
 
     qc.append(adder_gate, a + b + c)
-    qc.append(adder_gate_dag, N + b + c)
+    qc.append(adder_gate_dag, M + b + c)
     qc.x(b[-1])
     qc.cx(b[-1], t[0])
     qc.x(b[-1])
-    for i, char in enumerate(bin(N_val)[2:][::-1]):
+    for i, char in enumerate(bin(M_val)[2:][::-1]):
         if char == '1':
-            qc.cx(t[0], N[i])
-    qc.append(adder_gate, N + b + c)
-    for i, char in enumerate(bin(N_val)[2:][::-1]):
+            qc.cx(t[0], M[i])
+    qc.append(adder_gate, M + b + c)
+    for i, char in enumerate(bin(M_val)[2:][::-1]):
         if char == '1':
-            qc.cx(t[0], N[i])
+            qc.cx(t[0], M[i])
     qc.append(adder_gate_dag, a + b + c)
     qc.cx(b[-1], t[0])
     qc.append(adder_gate, a + b + c)
 
-    for i, char in enumerate(bin(N_val)[2:][::-1]):
+    for i, char in enumerate(bin(M_val)[2:][::-1]):
         if char == '1':
-            qc.x(N[i])
+            qc.x(M[i])
 
     return qc.to_gate()
 
-def adder_modN_hoge(N: int, N_len: Optional[int] = None) -> Gate:
-    N_val = N
-    if N_len is None:
-        N_len = int(np.ceil(np.log2(N)))
-
-    qubits = QuantumRegister(N_len + (N_len + 1) + N_len + N_len + 1)
-    a, left_qubits = qubits[:N_len], qubits[N_len:]
-    b, left_qubits = left_qubits[:N_len + 1], left_qubits[N_len + 1:]
-    c, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
-    N, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
-    t = left_qubits[:1]
-    qc = QuantumCircuit(qubits)
-
-    adder_gate = adder(N_len)
-    adder_gate_dag = adder_gate.inverse()
-
-    for i, char in enumerate(bin(N_val)[2:][::-1]):
-        if char == '1':
-            qc.x(N[i])
-    qc.append(adder_gate_dag, a + b + c)
-    return qc.to_gate()
-    qc.cx(b[-1], t[0])
-    qc.append(adder_gate, a + b + c)
-
-def ctrl_multi_modN(a: int, N: int) -> Gate:
-    N_val = N
-    N_len = int(np.ceil(np.log2(N)))
+def ctrl_multi_modM(a: int, M: int, N_len: int) -> Gate:
+    M_val = M
+    N_len = N_len
 
     qubits = QuantumRegister(9 * N_len - 1)
     ctrl, left_qubits = qubits[:1], qubits[1:]
@@ -120,19 +94,19 @@ def ctrl_multi_modN(a: int, N: int) -> Gate:
     y, left_qubits = left_qubits[:2 * N_len], left_qubits[2 * N_len:]
     xx, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
     c, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
-    N, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
+    M, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
     t, left_qubits = left_qubits[:1], qubits[1:]
 
     qc = QuantumCircuit(qubits)
 
-    adder_modN_gate = adder_modN(N=N_val, N_len=2 * N_len - 1)
+    adder_modM_gate = adder_modM(M=M_val, N_len=2 * N_len - 1)
 
     for i in range(N_len):
-        for j, char in enumerate(bin(2 ** i * a % N_val)[2:][::-1]):
+        for j, char in enumerate(bin(2 ** i * a % M_val)[2:][::-1]):
             if char == '1':
                 qc.ccx(ctrl[0], x[i], xx[j])
-        qc.append(adder_modN_gate, xx + y + c + N + t)
-        for j, char in enumerate(bin(2 ** i * a % N_val)[2:][::-1]):
+        qc.append(adder_modM_gate, xx + y + c + M + t)
+        for j, char in enumerate(bin(2 ** i * a % M_val)[2:][::-1]):
             if char == '1':
                 qc.ccx(ctrl[0], x[i], xx[j])
     qc.x(ctrl[0])
@@ -142,64 +116,30 @@ def ctrl_multi_modN(a: int, N: int) -> Gate:
 
     return qc.to_gate()
 
-def ctrl_multi_modN_hoge(a: int, N: int) -> Gate:
-    N_val = N
-    N_len = int(np.ceil(np.log2(N)))
-
-    qubits = QuantumRegister(9 * N_len - 1)
-    ctrl, left_qubits = qubits[:1], qubits[1:]
-    x, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
-    y, left_qubits = left_qubits[:2 * N_len], left_qubits[2 * N_len:]
-    xx, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
-    c, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
-    N, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
-    t, left_qubits = left_qubits[:1], qubits[1:]
-
-    qc = QuantumCircuit(qubits)
-
-    adder_modN_gate_dag = adder_modN(N=N_val, N_len=2 * N_len - 1).inverse()
-
-    qc.x(ctrl[0])
-    for x_bit, y_bit in zip(x, y):
-        qc.ccx(ctrl[0], x_bit, y_bit)
-    qc.x(ctrl[0])
-    for i in reversed(range(N_len)):
-        for j, char in enumerate(bin(2 ** i * a)[2:][::-1]):
-            if char == '1':
-                qc.ccx(ctrl[0], x[i], xx[j])
-        if i == 0:
-            adder_modN_gate_dag = adder_modN_hoge(N=N_val, N_len=2 * N_len - 1)
-        qc.append(adder_modN_gate_dag, xx + y + c + N + t)
-        if i == 0:
-            return qc.to_gate()
-        for j, char in enumerate(bin(2 ** i * a)[2:][::-1]):
-            if char == '1':
-                qc.ccx(ctrl[0], x[i], xx[j])
-
-def ax_modN(a: int, N: int) -> Gate:
-    N_val = N
-    N_len = int(np.ceil(np.log2(N)))
+def ax_modM(a: int, M: int, N_len: Optional[int] = None) -> Gate:
+    M_val = M
+    if N_len is None:
+        N_len = int(np.ceil(np.log2(M)))
 
     qubits = QuantumRegister(10 * N_len - 2)
     x, left_qubits = qubits[:N_len], qubits[N_len:]
-    x_for_ctrl_multi_modN_gate, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
+    x_for_ctrl_multi_modM_gate, left_qubits = left_qubits[:N_len], left_qubits[N_len:]
     y, left_qubits = left_qubits[:2 * N_len], left_qubits[2 * N_len:]
     xx, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
     c, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
-    N, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
+    M, left_qubits = left_qubits[:2 * N_len - 1], left_qubits[2 * N_len - 1:]
     t, left_qubits = left_qubits[:1], qubits[1:]
 
     qc = QuantumCircuit(qubits)
 
-    qc.x(x_for_ctrl_multi_modN_gate[0])
+    qc.x(x_for_ctrl_multi_modM_gate[0])
     for i in range(N_len):
-        ctrl_multi_modN_gate = ctrl_multi_modN(pow(a, 2 ** i, N_val), N_val)
-        ctrl_multi_modN_gate_dag = ctrl_multi_modN(pow(a, -2 ** i, N_val), N_val).inverse()
-        #ctrl_multi_modN_gate_dag = ctrl_multi_modN_hoge(pow(a, -2 ** i, N_val), N_val)
+        ctrl_multi_modM_gate = ctrl_multi_modM(pow(a, 2 ** i, M_val), M_val, N_len)
+        ctrl_multi_modM_gate_dag = ctrl_multi_modM(pow(a, -2 ** i, M_val), M_val, N_len).inverse()
 
-        qc.append(ctrl_multi_modN_gate, [x[i]] + x_for_ctrl_multi_modN_gate + y + xx + c + N + t)
+        qc.append(ctrl_multi_modM_gate, [x[i]] + x_for_ctrl_multi_modM_gate + y + xx + c + M + t)
         for j in range(N_len):
-            qc.cswap(x[i], x_for_ctrl_multi_modN_gate[j], y[j])
-        qc.append(ctrl_multi_modN_gate_dag, [x[i]] + x_for_ctrl_multi_modN_gate + y + xx + c + N + t)
+            qc.cswap(x[i], x_for_ctrl_multi_modM_gate[j], y[j])
+        qc.append(ctrl_multi_modM_gate_dag, [x[i]] + x_for_ctrl_multi_modM_gate + y + xx + c + M + t)
 
     return qc.to_gate()
--- qqz/shor.py
+++ qqz/shor.py
@@ -8,31 +8,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from qiskit import QuantumCircuit, Aer, transpile, assemble
 from qiskit.visualization import plot_histogram
+from sympy import Rational
+from sympy.ntheory.continued_fraction import continued_fraction, continued_fraction_convergents
 
 from qft import qft
-from elementary import ax_modN
+from elementary import ax_modM
 
 
-def shor(N: int, a: Optional[int] = None, show_hist=True, use_only_period=False):
+def shor(M: int, a: Optional[int] = None, show_hist=True, use_only_period=False):
     if a is None:
-        random.randint(2, N - 1)
-    gcd = math.gcd(a, N)
+        random.randint(2, M - 1)
+    gcd = math.gcd(a, M)
     if gcd != 1:
         print(f'Answer is found in only classical computation: {gcd}')
         return gcd
 
-    N_len = int(np.ceil(np.log2(N)))
-    qc = QuantumCircuit(10 * N_len - 2, 2 * N_len)
+    N_len = int(np.ceil(np.log2(M ** 2)))
+    N = 2 ** N_len
+    #qc = QuantumCircuit(10 * N_len - 2, 2 * N_len)
+    qc = QuantumCircuit(10 * N_len - 2, N_len)
     #qc = QuantumCircuit(10 * N_len - 2, 10 * N_len - 2)
 
     qc.h(range(N_len))
     #qc.x([0, 1])
 
-    qc.append(ax_modN(a=a, N=N), range(10 * N_len - 2))
+    qc.append(ax_modM(a=a, M=M, N_len=N_len), range(10 * N_len - 2))
 
     qc.append(qft(n=N_len), range(N_len))
 
-    qc.measure(range(2 * N_len), range(2 * N_len))
+    #qc.measure(range(2 * N_len), range(2 * N_len))
+    qc.measure(range(N_len), range(N_len))
     #qc.measure(range(10 * N_len - 2), range(10 * N_len - 2))
 
     backend = Aer.get_backend('aer_simulator_matrix_product_state')#('aer_simulator')
@@ -44,52 +49,31 @@ def shor(N: int, a: Optional[int] = None, show_hist=True, use_only_period=False)
         plot_histogram(hist)
         plt.show()
 
+    print(sorted(hist.items(), key=lambda x: x[1], reverse=True))
     y_list = []
-    for measured_key, _ in sorted(hist.items(), key=lambda x: x[1]):
+    for measured_key, _ in sorted(hist.items(), key=lambda x: x[1], reverse=True):
         y = int(measured_key[-N_len:], 2)
         if y == 0:
             continue
-        y_list.append(y)
 
-        y_per_N = Fraction(y, N)
-        maybe_r = y_per_N.denominator
-
-        if use_only_period:
-            if a ** maybe_r % N == 1:
-                return maybe_r
-            continue
-
-        if maybe_r % 2 == 1 or (a ** (maybe_r // 2) + 1) % N == 0:
-            continue
-        gcd = math.gcd(a ** (maybe_r // 2) + 1, N)
-        if gcd != 1:
-            return gcd
-        gcd = math.gcd(a ** (maybe_r // 2) - 1, N)
-        if gcd != 1:
-            return gcd
-    print(y_list)
-    for y1, y2 in combinations(y_list, 2):
-        y1_per_y2 = Fraction(y1, y2)
-        m1 = y1_per_y2.numerator
-        m2 = y1_per_y2.denominator
-        maybe_r12 = [N * m1 // y1, N * m2 // y2]
-        for maybe_r in maybe_r12:
-            if use_only_period:
-                if a ** maybe_r % N == 1:
+        for fraction in continued_fraction_convergents(continued_fraction(Rational(y, N))):
+            maybe_r = fraction.denominator
+            if maybe_r != 1 and maybe_r < M and a ** maybe_r % M == 1:
+                if use_only_period:
                     return maybe_r
-                continue
 
-            if maybe_r % 2 == 1 or (a ** (maybe_r // 2) + 1) % N == 0:
-                continue
-            gcd = math.gcd(a ** (maybe_r // 2) + 1, N)
-            if gcd != 1:
-                return gcd
-            gcd = math.gcd(a ** (maybe_r // 2) - 1, N)
-            if gcd != 1:
-                return gcd
+                if maybe_r % 2 == 1 or (a ** (maybe_r // 2) + 1) % M == 0:
+                    continue
+                gcd = math.gcd(a ** (maybe_r // 2) + 1, M)
+                if gcd != 1:
+                    return gcd
+                gcd = math.gcd(a ** (maybe_r // 2) - 1, M)
+                if gcd != 1:
+                    return gcd
 
     print('gcd was not found?!')
 
 if __name__ == '__main__':
-    #print(shor(N=8, a=3))
-    print(shor(N=7, a=3, use_only_period=True))
+    #print(shor(M=8, a=3))
+    print(shor(M=57, a=5))
+    #print(shor(M=7, a=3, use_only_period=True))
